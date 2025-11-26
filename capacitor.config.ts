f0bb811f09d7208939f3038f064f6bdddbf802ff
saibaba.padala <saibaba.padala@uncode.io>
2025-11-26 12:38:21 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'OnpoardingStore',
+  webDir: 'www'
+};
+
+export default config;
